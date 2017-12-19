d118e0187de975f0fe6ac0b5a070dcc7234405b6
Eugene Kwok <ekwok@reliaslearning.com>
2017-12-18 22:46:25 -0500

    hook up dashboard routing and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
+      }
+    ]
   }
 ];
 
