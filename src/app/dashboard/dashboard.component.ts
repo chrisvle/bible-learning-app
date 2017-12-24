b26d12e15411f93fc09a9a95c3d618a07aea110a
Eugene Kwok <ekwok@reliaslearning.com>
2017-12-24 01:40:59 -0500

    hook up initial dashboard service

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -8,31 +9,11 @@ export class DashboardComponent {
   gameType: string;
   oldOrNew: string;
   bibleBook: string;
+  gameTypes = [];
 
-  gameTypes = [
-    {
-      id: 0,
-      type: 'Chapter Only',
-      description: 'Get a random verse. Guess the chapter.'
-    },
-    {
-      id: 1,
-      type: 'Chapter and Verse',
-      description: 'Get a random verse. Guess the chapter and the verse.'
-    },
-    {
-      id: 2,
-      type: 'Fill in the Blank',
-      description: 'Get a random verse. Fill in the Blank.'
-    },
-    {
-      id: 3,
-      type: 'Matching',
-      description: 'Get some random verses. Match the verse with the right chapter.'
-    }
-  ];
-
-  constructor() { }
+  constructor(private dashboardService: DashboardService) {
+    this.gameTypes = dashboardService.getGameTypes();
+  }
 
   setGameType(id: number): void {
     this.gameType = this.gameTypes[id].type;
