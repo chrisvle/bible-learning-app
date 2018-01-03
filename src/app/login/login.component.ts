f230844aa41b95a6fe11ae9ff22234033c801977
Eugene Kwok <ekwok@reliaslearning.com>
2018-01-02 23:09:43 -0500

    initial firebase google auth

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  signInWithGoogle() {
+    this.authService.signInWithGoogle();
+    this.router.navigate(['/dashboard']);
+  }
+}
