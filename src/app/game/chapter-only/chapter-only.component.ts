c0c1cf1b99bf4c37a45435c9c0026997f4ee0b0e
Eugene Kwok <ekwok@reliaslearning.com>
2018-01-07 20:24:41 -0500

    chapter-only in progress

--- src/app/game/chapter-only/chapter-only.component.ts
+++ src/app/game/chapter-only/chapter-only.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { BibleService } from '../../shared/services/bible.service';
 
 @Component({
   templateUrl: './chapter-only.component.html',
@@ -12,9 +14,13 @@ export class ChapterOnlyComponent implements OnInit {
   correctAnswers = 0;
   totalQuestions = 0;
 
-  constructor(private ar: ActivatedRoute, private router: Router) {
+  chapterNumber$: Observable<number>;
+  randomVerse$: any;
+
+  constructor(private ar: ActivatedRoute, private router: Router, private bibleService: BibleService) {
     this.book = this.ar.snapshot.queryParams['book'];
     this.router.navigateByUrl('/game/chapter-only');
+    this.randomVerse$ = bibleService.getRandomVerse(this.book);
   }
 
   ngOnInit() {
--- src/app/shared/services/bible.service.ts
+++ src/app/shared/services/bible.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import { Testaments } from '../enums/testaments.enum';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { BibleBook } from '../models/bible-book.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
 
 @Injectable()
 export class BibleService {
 
   private bibleBooks$: FirebaseListObservable<BibleBook[]>;
+  // private esv$: FirebaseListObservable<any[]>;
+  private bibleBook$: FirebaseListObservable<any[]>;
+
+  private numberOfChapters$: Observable<number>;
+  private numberOfChapters: number;
+
+  private randomVerse$: any;
 
   constructor(private db: AngularFireDatabase) {
     this.bibleBooks$ = this.db.list('bible-books');
+    // this.esv$ = this.db.list('esv');
   }
 
   getOldTestamentBooks() {
@@ -22,5 +32,21 @@ export class BibleService {
     return this.bibleBooks$.map(books =>
       books.filter(book => book.testament === Testaments.New)
     );
+  }
+
+  getNumberOfChapters(bibleBook: string): Observable<number> {
+    return this.bibleBooks$.map(books =>
+      books.filter(book => book.name === bibleBook)[0].chapters
+    );
+  }
+
+  getRandomVerse(bibleBook: string) {
+    // this works!!
+    // return this.esv$.map(bible => bible.filter(esv => esv.$key === 'Matthew')[0][9][1]);
+    this.bibleBook$ = this.db.list(`esv/${bibleBook}`);
+    return this.bibleBook$;
+
+    // this.randomVerse$ = this.db.object(`esv/${bibleBook}/9/1`);
+    // return this.randomVerse$;
   }
 }
