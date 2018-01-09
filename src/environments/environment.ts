2ab09afed0265f79161e1bae8c9de35c10560b2c
Eugene Kwok <ekwok@reliaslearning.com>
2018-01-09 09:11:18 -0500

    add more bootstrap responsive breakpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDrWwfZi2599OOob4Ni2UHFMQuszXz8xyM',
+    authDomain: 'bible-learning-app.firebaseapp.com',
+    databaseURL: 'https://bible-learning-app.firebaseio.com',
+    projectId: 'bible-learning-app',
+    storageBucket: 'bible-learning-app.appspot.com'
+  },
+  restrictedEmailDomain: '@gpmail.org'
 };
