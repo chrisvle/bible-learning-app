f98764b0ad54e571250b0cfccef4cd32add3ea98
Eugene Kwok <eugenekwok11@gmail.com>
2018-01-09 09:42:20 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyDrWwfZi2599OOob4Ni2UHFMQuszXz8xyM',
-    authDomain: 'bible-learning-app.firebaseapp.com',
-    databaseURL: 'https://bible-learning-app.firebaseio.com',
-    projectId: 'bible-learning-app',
-    storageBucket: 'bible-learning-app.appspot.com'
-  },
-  restrictedEmailDomain: '@gpmail.org'
+  production: false
 };
